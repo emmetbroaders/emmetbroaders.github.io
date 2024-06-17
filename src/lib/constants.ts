de35225fb56ee12e88669147d8c10e2c0c40cb63
Emmet Broaders <emmetbroaders@gmail.com>
2024-06-17 17:45:23 +0100

    init

---
+++ src/lib/constants.ts
@@ -0,0 +1,15 @@
+export const LINKS = {
+  linkedin: "https://www.linkedin.com/in/emmetbroaders/",
+  github: "https://github.com/emmetbroaders",
+  mastodon: "https://mastodon.social/@broaders"
+
+  // medium: "https://ladvace.medium.com/",
+  // discord: "https://discordapp.com/users/163300027618295808",
+  // dribble: "https://dribbble.com/Ladvace_Jace",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
---
+++ src/lib/helpers.ts
@@ -0,0 +1,34 @@
+export function trimText(input: string, maxLength: number = 100): string {
+  if (input.length <= maxLength) return input;
+  return input.substring(0, maxLength - 3) + "...";
+}
+export function getCurrentTimeInUK(): Date {
+  // Create a date object with the current UTC time
+  const now = new Date();
+  return now;
+}
+
+export function formatTimeForUK(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZone: "Europe/London",
+  };
+
+  let formattedTime = new Intl.DateTimeFormat("en-GB", options).format(date);
+
+  // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
+  // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
+  formattedTime += " GMT";
+
+  return formattedTime;
+}
+
+export function formatDate(date: Date): string {
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
